75bc8f8cb6aa8e8803eb84cdb183fbd585ae5a85
eunbeee <dmsql2330@naver.com>
2022-10-19 19:22:25 +0900

    ASP.NET Core 6.0 Welcome

---
+++ DotNetNote/DotNetNote/Srartup.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DotNetNote
+{
+    public class Srartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
